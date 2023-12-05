18b4318ab2e5392d769a40a56eb41c300c52e222
Khalil-Khe1 <khemiri2000@gmail.com>
2023-12-05 12:15:08 +0100

    sort + search + notfiy + code clean up

--- frontend-Angular/src/app/moniteur/components/list/list.component.ts
+++ frontend-Angular/src/app/moniteur/components/list/list.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
-import {Service} from "../../../services/moniteur.service";
-import {Moniteur} from "../../../models/moniteur";
+import { Service } from "../../../services/moniteur.service";
+import { Moniteur } from "../../../models/moniteur";
 
 @Component({
   selector: 'list-component',
@@ -9,14 +9,16 @@ import {Moniteur} from "../../../models/moniteur";
 })
 export class ListComponent {
 
-  listAll : Moniteur[] = [] ;
-  constructor(private service : Service) {
+  listDefault: Moniteur[] = [];
+  listAll: Moniteur[] = [];
+  constructor(private service: Service) {
   }
 
   ngOnInit() {
     this.service.getAll().subscribe(
       (res: Moniteur[]) => {
-        this.listAll = res;
+        this.listDefault = res;
+        this.listAll = this.listDefault;
       },
       (error) => {
         console.error('Error fetching moniteurs:', error);
@@ -24,13 +26,55 @@ export class ListComponent {
     );
   }
 
-  remove(id : number) {
-      this.service.remove(id).subscribe(
-        (res) => {
-              if (res.status === 200)
-              this.listAll = this.listAll.filter(moniteur => moniteur.numMoniteur !== id);
+  remove(id: number) {
+    this.service.remove(id).subscribe(
+      (res) => {
+        console.log(res)
+          
 
-        }
-      )
+      }
+    )
+    this.listAll = this.listAll.filter(moniteur => moniteur.numMoniteur !== id);
   }
+
+  sortAsc() {
+    this.listAll.sort((a, b) => {
+      const date1 = new Date(a.dateRecru).getTime();
+      const date2 = new Date(b.dateRecru).getTime();
+      return date1 - date2;
+    });
+  }
+
+  sortDesc() {
+    this.listAll.sort((a, b) => {
+      const date1 = new Date(a.dateRecru).getTime();
+      const date2 = new Date(b.dateRecru).getTime();
+      return date2 - date1;
+    })
+  }
+
+  handleSearch(event: Event) {
+    const inputElement = event.target as HTMLInputElement;
+    const prompt = inputElement.value.toLocaleLowerCase();
+    console.log(prompt);
+    if (prompt.localeCompare("") == 0) {
+      this.listAll = this.listDefault;
+    } else {
+      this.listAll = this.listAll.filter((item) =>
+      (
+        (item.nomM.toLocaleLowerCase().includes(prompt))
+        || (item.prenomM.toLocaleLowerCase().includes(prompt))
+        || (item.dateRecru.toString().toLocaleLowerCase().includes(prompt))
+        || (item.numMoniteur.toString().toLocaleLowerCase().includes(prompt))
+      ));
+    }
+
+    /*(
+        (item.nomM.localeCompare(prompt))
+        || (item.prenomM == prompt)
+        || (item.dateRecru.toString() == prompt)
+        || (item.numMoniteur.toString() == prompt)
+      ) */
+  }
+
 }
