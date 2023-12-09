430f423dd00b573c5274380559c2116617acb2f6
ineslachkhem <ines.lachkhe@esprit.tn>
2023-12-09 04:28:54 -1000

    creat/list

--- frontend-Angular/src/app/component_ab/abonnement.module.ts
+++ frontend-Angular/src/app/component_ab/abonnement.module.ts
@@ -6,8 +6,6 @@ import { CommonModule } from '@angular/common';
 ;
 import { AbonnementListComponent } from './components/abonnement-list/abonnement-list.component';
 import { AbonnementCreateComponent } from './components/abonnement-create/abonnement-create.component';
-import { AbonnementUpdateComponent } from './components/abonnement-update/abonnement-update.component';
-import { AbonnementDeleteComponent } from './components/abonnement-delete/abonnement-delete.component';
 import { AbonnementRoutingModule } from './abonnement-routing.module';
 import { ReactiveFormsModule } from '@angular/forms';
 
@@ -17,8 +15,7 @@ import { ReactiveFormsModule } from '@angular/forms';
   declarations: [
     AbonnementListComponent,
     AbonnementCreateComponent,
-    AbonnementUpdateComponent,
-    AbonnementDeleteComponent
+    
 
 
   ],
--- frontend-Angular/src/app/component_ab/components/abonnement-create/abonnement-create.component.ts
+++ frontend-Angular/src/app/component_ab/components/abonnement-create/abonnement-create.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { FormGroup, Validators } from '@angular/forms';
+import { FormBuilder } from '@angular/forms';
+
 
 @Component({
   selector: 'app-abonnement-create',
@@ -6,5 +9,18 @@ import { Component } from '@angular/core';
   styleUrls: ['./abonnement-create.component.scss']
 })
 export class AbonnementCreateComponent {
+
+ abonnementForm: FormGroup;
+ constructor(private formBuilder :FormBuilder ){}
+  ngOnInit() {
+    // Initialisation du formulaire réactif avec des champs et des validateurs
+    this.abonnementForm = this.formBuilder.group({
+      dateDebyt: ["", [Validators.required]],
+      dateFin: ["", [Validators.required]],
+      prixAbon: ["", [Validators.required]],
+      typeAbonnement: ["", [Validators.required]]
+    });
+  }
+
 
 }
