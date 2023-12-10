9f7dcc88b85efc3061841b924f59f8445708dff2
ineslachkhem <ines.lachkhe@esprit.tn>
2023-12-09 14:45:44 -1000

    creat abon

--- frontend-Angular/src/app/services/abonnement.service.ts
+++ frontend-Angular/src/app/services/abonnement.service.ts
@@ -1,9 +1,31 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Abonnement } from '../models/abonnement';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AbonnementService {
+  baseUrl : string = "http://localhost:8089/Abonnement/" ;
 
-  constructor() { }
+  constructor(private _http:HttpClient) { }
+  getAbonnement(id:String):Observable<Abonnement>{
+
+    return this._http.get<Abonnement>(this.baseUrl+"get/"+id);
+  }
+  getAllAbonnement(): Observable<Abonnement[]> {
+    return this._http.get<Abonnement[]>(this.baseUrl + 'getall');
+  }
+  addAbonnement(abonnement : Abonnement): Observable<Abonnement> {
+    return this._http.post<Abonnement>(this.baseUrl+"add",abonnement);
+  }
+  updateAbonnement(abonnement : Abonnement): Observable<Abonnement> {
+    return this._http.put<Abonnement>(this.baseUrl+"update",abonnement);
+  }
+  deleteAbonnement(numAbon : number): Observable<any> {
+    return this._http.delete<any>(this.baseUrl+"delete/"+numAbon,   { observe: 'response' });
+  }
+
+
 }
